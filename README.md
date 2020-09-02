18cf06c31c6b2cfb6df7f4fa7671e854fae91f13
furoe <17751038730@163.com>
2020-09-02 16:19:39 +0800

    移除元素

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # leetcode-solution
 practice
 - [x] 无重复最长子串长度
+- [x] 移除元素
--- medium/findLenOfLongestSubstr.js
+++ medium/findLenOfLongestSubstr.js
@@ -25,3 +25,16 @@ var lengthOfLongestSubstring = function(s){
 	}
 	return len;
 }
+
+var lengthOfLongestSubstring1 = function(s){
+	let arr = [], max = 0
+    for(let i = 0; i < s.length; i++) {
+        let index = arr.indexOf(s[i])
+        if(index !== -1) {
+            arr.splice(0, index+1);
+        }
+        arr.push(s.charAt(i))
+        max = Math.max(arr.length, max) 
+    }
+    return max;
+}
